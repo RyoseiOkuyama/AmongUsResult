e5fd9c61f6df3b46b9217bd455db6746ac236dcc
Ryosei Okuyama <ryo.o.18.a@gmail.com>
2022-04-25 20:14:42 +0900

    heroku

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,34 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'ResultController@index');
+
+Route::get('/communities/community_create', 'CommunityController@CreateCommunity');
+Route::post('/communities/created', 'CommunityController@CreatedCommunity');
+Route::get('/communities/community_index', 'CommunityController@index');
+Route::get('/communities/{community}', 'CommunityController@ShowCommunity');
+
+Route::get('/regulations/regulation_create/{community}', 'CommunityController@CreateRegulation');
+Route::post('/regulations/created', 'CommunityController@CreatedRegulation');
+Route::get('/regulations/{regulation}', 'CommunityController@ShowRegulation');
+
+Route::get('/players/player_create/{community}', 'CommunityController@CreatePlayer');
+Route::post('/players/created', 'CommunityController@CreatedPlayer');
+Route::get('/players/{player}', 'CommunityController@ShowPlayer');
+
+
+Route::get('/results/result_create1', 'ResultController@create1');
+Route::get('/results/result_create2', 'ResultController@create2');
+Route::get('/results/result_create3', 'ResultController@create3');
+Route::get('/results/result_create4', 'ResultController@create4');
+Route::post('/results/result_created', 'ResultController@created');
+Route::get('/results/{result}', 'ResultController@show');
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
